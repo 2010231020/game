6226324379e44e5425d6a7cc62c709d91e7e47f7
kbaoqq <389565552@qq.com>
2017-08-18 16:32:03 +0800

    delete wing libs

--- libs/exml.e.d.ts
+++
@@ -1,8 +0,0 @@
-declare class GameSceneSkin extends eui.Skin{
-}
-declare class MainSceneSkin extends eui.Skin{
-}
-declare class ShipSkin extends eui.Skin{
-}
-declare class ShipSkin2 extends eui.Skin{
-}
